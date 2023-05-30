5a73fd11af6cbb7720ac6f4d5b9ba4f786a4f441
gaaganastasia <n.young.blood@mail.ru>
2023-05-30 14:33:43 +0700

    ms

--- controllers/orders.js
+++ controllers/orders.js
@@ -19,8 +19,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+    user: 'fornodemailsender@mail.ru',
+    pass: 'qZW9Z4s2UUGryGvHRipf'
   }
 });
 
